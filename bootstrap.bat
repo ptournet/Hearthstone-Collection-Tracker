bcac0e3a77d83fec81857fa8fd2ce403a506bc7f
judge2020 <judge2020@judge2020.com>
2017-08-07 15:35:18 -0400

    Fix bootstrap

--- bootstrap.bat
+++ bootstrap.bat
@@ -1,2 +1,11 @@
+@echo off
 git clone https://github.com/HearthSim/HearthMirror
 git clone https://github.com/HearthSim/HearthDb
+
+nuget help | head -1  >nul 2>&1 && (
+    nuget help | head -1
+    nuget restore
+) || (
+    ECHO nuget is not installed. When loading project, go to VIEW -> other windows -> package manager console and restore nuget packages
+    pause
+)
